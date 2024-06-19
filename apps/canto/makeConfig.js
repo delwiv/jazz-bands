af2c37ab1a609e03f5acbb96beb24f3de8bbda4a
Louis Cathala <delwiv@protonmail.com>
2024-06-19 15:59:02 +0200

    feat: initial commit, working basic docker and compose stack, need to test certbot and nginx stuff on server

---
+++ apps/canto/makeConfig.js
@@ -0,0 +1,40 @@
+const { writeFile, readFile } = require('node:fs/promises')
+const { resolve } = require('node:path')
+
+const env = process.env
+
+const {
+  MONGODB_USERNAME,
+  MONGODB_PASSWORD,
+  MONGODB_DATABASE,
+  MONGODB_HOST,
+} = env
+
+const [host, port] = (MONGODB_HOST || '').split(':')
+
+const mkConfig = async () => {
+  await writeFile(resolve('./server', 'datasources.json'), JSON.stringify(
+    {
+      db: {
+        name: "db",
+        connector: "memory"
+      },
+      mongoDB: {
+        host: host || 'localhost',
+        database: MONGODB_DATABASE || 'canto',
+        username: MONGODB_USERNAME,
+        password: MONGODB_PASSWORD,
+        name: "mongoDB",
+        connector: "mongodb"
+      },
+      storage: {
+        name: "storage",
+        connector: "loopback-component-storage",
+        provider: "filesystem",
+        root: "server/storage"
+      }
+    }
+    , null, 2))
+}
+
+mkConfig()
