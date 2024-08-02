97cbf25a5bc93e8a9ca285671c6b804e0eec29d6
Delwiv <delwiv@protonmail.com>
2024-08-02 13:02:44 +0000

    feat: update getToken and gmail

--- apps/managr-api/getToken.js
+++ apps/managr-api/getToken.js
@@ -29,7 +29,7 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed
+  const { client_secret, client_id, redirect_uris } = credentials.web
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
  return getNewToken(oAuth2Client, callback)
   // Check if we have previously stored a token.
