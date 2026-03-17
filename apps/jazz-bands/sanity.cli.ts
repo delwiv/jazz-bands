8f491cb6d31cd2437edf766c2c280005d770d4c8
Louis Cathala <delwiv@protonmail.com>
2026-03-17 13:28:10 +0100

    refactor: remove default fallbacks in sanity.cli.ts

--- apps/jazz-bands/sanity.cli.ts
+++ apps/jazz-bands/sanity.cli.ts
@@ -3,7 +3,7 @@ import { SANITY_PROJECT_ID, SANITY_DATASET } from './lib/sanity-settings'
 
 export default defineCliConfig({
   api: {
-    projectId: SANITY_PROJECT_ID || '94fpfdn8',
-    dataset: SANITY_DATASET || 'staging',
+    projectId: SANITY_PROJECT_ID,
+    dataset: SANITY_DATASET,
   },
 })
